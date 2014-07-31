d4b9ce8373eb8ddfd53d56b402d74785c776e937
Minh Vu <minvu@pdx.edu>
2014-07-30 22:57:30 -0700

    Update Proxy

--- proxy_v2.py
+++ proxy_v2.py
@@ -11,12 +11,18 @@ import email, imaplib, re, StringIO, ConfigParser, base64, sys
 from encryption import *
 
 header_part = ['From', 'To', 'Subject']
+
 Tokenize_append = "APPEND"
 Tokenize_Login = "LOGIN"
 Tokenize_authenticate = "authenticate plain"
 Tokennize_capability = "capability"
+Tokennize_A = "@"
+Tokenize_Select = "select"
+Tokenize_Inbox = '"INBOX"'
+
 config = ConfigParser.ConfigParser()
 config.read("/home/minhvu/Desktop/ssee/config_proxy")
+
 
 class ServerProtocol(protocol.Protocol):
 	
@@ -25,15 +31,14 @@ class ServerProtocol(protocol.Protocol):
 	# Using self.email_address to get the whole user's email_address 
 	# Using self.flag to know which data we need
 
-	def __init__(self, server):
+	def __init__(self):
 		self.client = None
-		#self.other = Other_Function()
-		self.email_address = None
 		self.buffer = None
 		self.flag_Login = False
 		self.flag_Append = False
-		self.server = server
 		self.d = defer.Deferred()
+		self.other = None
+		self.folder_selection = None
 
 	# Create a connection between the first part and the second part of proxy
 
@@ -41,28 +46,35 @@ class ServerProtocol(protocol.Protocol):
 		factory = protocol.ClientFactory()
 		factory.protocol = ClientProtocol
 		factory.server = self
-		reactor.connectTCP(self.server, SERVER_PORT, factory)
+		reactor.connectTCP(self.factory.SERVER_ADDR, SERVER_PORT, factory)
 
 	# The gate Client sending data to Proxy
 	def dataReceived(self, data):
 		print "C->S: %s", repr(data)
 
 		if self.client:
-			print "hehehehe"
 
+			# Login function to get data 
+			user_address = '' 
 			if Tokenize_authenticate in data.upper():
 				# Open the flag to get the new message
 				self.flag_Login = True
 			elif self.flag_Login == True:
-				# Using base64 to decrypt information
-				self.email_address = base64.b64decode(
-					data.replace('\r\n','')).split('\x00')[1] + '@' + SERVER_ADDR
-				self.flag_Login = False
+				# Using base64 to decrypt information, bc IMAP extension using 
+				# base64 to encode data "Authenticate Command"
+				user_address = base64.b64decode(
+					data.replace('\r\n','')).split('\x00')[1]
 			elif Tokenize_Login in data.upper() and len(data.split(' ')) == 4:
+				user_address = data.split(' ')[-2]
+			if not Tokennize_A in data:
+				#check if user_address is right or wrong
+				user_address = user_address + '@ubuntu.com'
+			#create prf_key and cipher_key
+			self.other = Other_Functions(user_address)
+			self.flag_Login = False
 
 			# Appeding data to mail box
-
-			if self.Tokenize_append or self.Tokenize_append.lower() in data:
+			if Tokenize_append in data.upper():
 				self.flag_Append = True
 				self.buffer += data
 				return
@@ -70,61 +82,36 @@ class ServerProtocol(protocol.Protocol):
 			elif self.flag_Append == True:
 				if len(data) == 2:
 					self.flag_Append = False
-					self.buffer += data
-					transfer_email = self.other.Append(self.buffer)
+					self.client.write(self.other.Append(self.buffer))
+					self.client.write('\r\n')
+					return
 				else:
 					self.buffer += data
 					return
 
+			if Tokenize_Select in data.lower():
+				self.folder_selection = data.split(' ')[-2]
+
 			self.client.write(data)
 		else:						
 			self.buffer = data
-			print "hohohoho"
-			# login function
-			
-				# Some Email clients do not encrypted string, solving easily
-				if "@" in data:
-					self.server = config.get('Server', data.split(' ')[-2].split("@")[-1])
-				else:
-					self.server = config.get('Server', 'ubuntu.com')
-
-				factory.protocol = ClientProtocol
-				factory.server = self
-				reactor.connectTCP(self.server, SERVER_PORT, factory)
-
-				self.client.write(self.buffer)
-
-			if Tokennize_capability in data.lower() and len(data.split(' ')) == 2:
-				uid_command = data.split(' ')[0]
-				curr_data_1 = config.get('Fake_Command', 'capability')
-				curr_data_1 = curr_data_1.replace(curr_data_1.split('\\r\\n')[1].split(' ')[0]
-					, uid_command)
-				self.transport.write(curr_data_1.replace('\\r\\n', '\r\n') + '\r\n')
-				self.buffer = None
-
-
 
 	# The gate Proxy sending data to Client
 	def write(self, data):
 		print "S->C: %s", repr(data)
 
-		'''if "FETCH" and "OK Fetch completed" and "BODY[]" in data:
-			data = self.other.Fetch(data, self.cipher_key)
-		elif "FETCH" and "RFC822.SIZE" in data:
-			print "hehehe"
-			data = self.other.Preview(data, self.cipher_key)'''
+		print self.folder_selection
+
+		if (self.folder_selection == Tokenize_Inbox) and (
+			"FETCH" and "OK Fetch completed" and "BODY[]" in data):
+			data = self.other.Fetch(data)
+
+		'''elif (self.folder_selection == Tokenize_Inbox) and (
+			"FETCH" and "RFC822.SIZE" in data):
+			#data = self.other.Preview(data, self.cipher_key)
+			return'''
 
 		self.transport.write(data)
-
-	def sending_append(self, append_cmd, transfer_email):
-		for i in range(3):
-			if i == 0:
-				self.client.write(append_cmd)
-			elif i == 1:
-				self.client.write(transfer_email)
-			else:
-				self.client.write('\r\n')
-		return
 
 
 class ClientProtocol(protocol.Protocol):
@@ -142,13 +129,36 @@ class ClientProtocol(protocol.Protocol):
 		if data:
 			self.transport.write(data)
 
+class Other_Functions(object):
+	def __init__(self, email_address):
+		self.E = Encryption(config.get('Proxy', 'pass_proxy'), email_address)
+
+	def Append(self, data):
+		plain_email = email.message_from_string('\r\n'.join(data.split('\r\n')[1:]))
+		enc_email = str(self.E.encrypt_email(plain_email)).replace('\n', '\r\n')
+		append_cmd = self.Changing_Length(data.split('\r\n')[0] + '\r\n'
+			, len(enc_email))
+		return append_cmd + enc_email
+
+	def Changing_Length(self, data, new_length):
+		number = data.split("{")[-1].replace("}\r\n", "")
+		data = data.replace(str(number), str(new_length) + '+')
+		return data
+
+	def Fetch(self, data):
+		enc_email = email.message_from_string('\r\n'.join(data.split('\r\n')[1:]))
+		plain_email = str(self.E.decrypt_email(enc_email).replace('\n', '\r\n'))
+		print repr(plain_email)
+
+
 def main(argv):
-	print argv
 	factory = protocol.ServerFactory()
 	if len(argv) == 1:
-		factory.protocol = ServerProtocol(config.get('Server', argv[0]))
+		factory.SERVER_ADDR = config.get('Server', argv[0])
 	else:
-		print "I don't understand which server you want to connect"
+		factory.SERVER_ADDR = 'localhost'
+	factory.protocol = ServerProtocol
+
 
 	reactor.listenTCP(LISTEN_PORT, factory)
 	reactor.run()
